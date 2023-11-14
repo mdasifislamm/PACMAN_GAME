3264316325e20e9d5d2ceca7aab68f74b19a4262
Shifa Haque <shifaaahaque@gmail.com>
2023-11-14 13:00:17 +0600

    initial and start

--- Game.java
+++ Game.java
@@ -24,3 +24,34 @@ public Game() {
         heart = new ImageIcon("res/heart.png").getImage();
 
     }
+private void initializeVariables() {
+
+    screenData = new int[NUM_BLOCK * NUM_BLOCK];
+    dim = new Dimension(400, 400);
+    ghostX = new int[MAX_GHOSTS];
+    ghostDX = new int[MAX_GHOSTS];
+    ghostY = new int[MAX_GHOSTS];
+    ghostDY = new int[MAX_GHOSTS];
+    ghostSpeed = new int[MAX_GHOSTS];
+    dx = new int[4];
+    dy = new int[4];
+
+    timer = new Timer(40, this);
+    timer.start();
+}
+
+private void startGame(Graphics2D g) {
+
+    if (pacmanAlive) {
+
+        death();
+
+    } else {
+
+        movePacman();
+        drawPacman(g);
+        moveGhosts(g);
+        checkMaze();
+    }
+}
+
