7c0145b96fe674fda85908c4bc04e4185c1f1180
Md Asif Islam <lionelasif@outlook.com>
2023-11-14 12:56:09 +0600

    addimages

--- Game.java
+++ Game.java
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 public Game() {
     addImages();
     initializeVariables();
@@ -5,3 +12,15 @@ public Game() {
     setFocusable(true);
     initializeGame();
 }
+
+
+
+    private void addImages() {
+    	down = new ImageIcon("res/down.gif").getImage();
+    	up = new ImageIcon("res/up.gif").getImage();
+    	left = new ImageIcon("res/left.gif").getImage();
+    	right = new ImageIcon("res/right.gif").getImage();
+        ghost = new ImageIcon("res/ghost.gif").getImage();
+        heart = new ImageIcon("res/heart.png").getImage();
+
+    }
